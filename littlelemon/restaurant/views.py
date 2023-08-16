2efebe0bf9c53f54dc42f7b96246ded1bb82577d
QY0831 <cqy95@qq.com>
2023-08-16 13:58:41 +0800

    update

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Booking, Menu
@@ -15,14 +15,27 @@ def index(request):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated, IsAdminUser]
+        if self.request.method == "GET":
+            permission_classes = []
+        return [permission() for permission in permission_classes]
 
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated]
+        if self.request.method in ["PATCH", "DELETE"]:
+            permission_classes = [IsAuthenticated, IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
+
